616466b3420fd95f976a71db95ca1d395f608166
Jonathan Cao <jcow77@gmail.com>
2017-12-23 08:57:28 -0500

    added methods quoteMaxAmount and arbitrage
    assumed that the arbitraged amount was with market price and the entire quoted currency's balance

--- arbitrage.py
+++ arbitrage.py
@@ -1,20 +1,40 @@
 import ccxt
-
-#prints all of exchanges listed in the API
-print(ccxt.exchanges)
-
+import time
+#test exchanges: poloniex binance gate cryptopedia and gdax
 #Loads the market for each exchange
-poloniex=ccxt.poloniex()
-gate=ccxt.gateio()
+poloniex = ccxt.poloniex({"apikey": null, 'secret': null})
+gate = ccxt.gateio({"apikey": null, 'secret': null})
 #save market data
 #market data has the format of:
 #{maker,taker,precision{amount,price},limits{amount{min,max}},price{min,max},cost{min,max}}
-poloniexMarkets=poloniex.load_markets()
-gateMarkets=gate.load_markets()
+poloniexMarkets = poloniex.load_markets()
+gateMarkets = gate.load_markets()
+
 #prints id with market
-poloniexSymbols=poloniex.symbols
-gateSymbols=gate.symbols
+poloniexSymbols = poloniex.symbols
+gateSymbols = gate.symbols
+
 print(poloniexSymbols)
 print(gateSymbols)
+
+def quoteMaxAmount(exchange, pair, price):
+    balance = exchange.fetchBalance() #would be best to store in array
+    quoteBalance = balance['free'][exchange.markets[pair]['quote']]
+    amount = quoteBalance / price
+    return amount
+
+def arbitrage(exchangeA, exchangeB, pair):
+    maxBought = quoteMaxAmount(exchangeA, pair, 0)
+    exchangeA.createMarketBuyOrder(pair, maxBought)
+    buyInfo = exchangeA.fetchOrders (symbol = pair, limit = 1) #most recent order
+    if(buyInfo['status'] == 'closed'):
+        exchangeA.withdraw(exchangeA.markets[pair]['quote'], none)
+        if('''withdrawal is done'''):
+            exchangeB.createMarketSellOrder(pair, exchangeB.markets[pair]['base'])
+
+
+
+
+
 
 
